eb6d67213ae23d1be71c3b179c5a21db9c3d7730
krishan7233 <kmpandey7233@gmail.com>
2023-10-14 18:34:04 +0530

    added ui latest code

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -15,7 +15,17 @@
 <!-- Plugins JavaScripts -->
 <script src="{{ asset('assets/js/plugins/plugins.js') }}"></script>
 
+
+
+
+
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+
+
+
 <script>
 	$('.seconddate-toggle').click(function() {
   $('.seconddate').slideToggle();
@@ -221,6 +231,38 @@ $("#singledob").datepicker({ dateFormat:'dd/mm/yy'});
       });
     });
 </script>
+
+<!--Our Partners start-->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+<script>
+
+	$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+</script>
+
+<!--Our Partners End-->
+
 
 </body>
 </html>
--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -1,25 +1,45 @@
 <!-- Nav Bar Starts here -->
+
 <div class="main-header">
-<nav class="navbar fixed-top navbar-expand-lg">
-  <div class="container">
-    <div class="navbar-light">
-      <!-- Logo -->
-      <!--<a class="navbar-brand" href="#"> <img src="images/head_logo.png" alt=""> </a>-->
-      <!-- Logo -->
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-               aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="#">About Us</a> </li>
-           <li class="nav-item"> <a class="nav-link" href="{{ route('super-visa') }}">Super Visa Insurance</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="#">Contact</a> </li>
-        </ul>
-       
+  <nav class="navbar fixed-top navbar-expand-lg">
+    <div class="container">
+      <div class="navbar-light bg-dark">
+        <!-- Logo -->
+        <a class="navbar-brand" href="index.html"> <img src="assets/images/logo.png" alt=""> </a>
+        <!-- Logo -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
+            <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Travel Insurance </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdown"> 
+				  <a class="dropdown-item" href="{{ route('super-visa') }}">Super Visa Insurance</a> 
+				  <a class="dropdown-item" href="#">Visitor To Canada Insurance</a> 
+				  <a class="dropdown-item" href="#">Canadian Travelling Out of Country / Province</a> 
+			  </div>
+            </li>
+			
+			<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Investments </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdown"> 
+				  <a class="dropdown-item" href="#">RESP</a> 
+				  <a class="dropdown-item" href="#">RRSP</a> 
+				  <a class="dropdown-item" href="#">TFSA</a> 
+			  </div>
+            </li>
+			
+			<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Resource </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown" id="navbarDropdown"> 
+				  <a class="dropdown-item" href="#">Blog</a> 
+			  </div>
+            </li>
+			<li class="nav-item"> <a class="nav-link" href="#">FAQs</a> </li>
+			<li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#">Contact</a> </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </div>
 <div class="clear-fix"></div>
 <!-- Nav Bar Ends here -->
